363c77b07ba8688cc5192f4339faf4d3097cc109
AslanRay <aslan_ray@icloud.com>
2019-01-16 17:30:04 -0800

    Dashboard provicional de la app

---
+++ TestBridge/app/src/main/java/com/jorgesoto/testbridge/Dashboard.kt
@@ -0,0 +1,16 @@
+package com.jorgesoto.testbridge
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+
+class Dashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+    }
+}
